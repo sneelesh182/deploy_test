5f974d227cf4fd6be1f597d003cf7dafacff723c
sneelesh182 <166792688+sneelesh182@users.noreply.github.com>
2024-08-19 18:30:38 +0530

    documentation route changed

--- eval/src/swagger.js
+++ eval/src/swagger.js
@@ -9,7 +9,7 @@ const options = {
     },
     servers:[
         {
-            url:'http://localhost:3000',
+            url:'https://deploy-test-msnv.onrender.com',
             description:'Event Management App'
         }
     ]

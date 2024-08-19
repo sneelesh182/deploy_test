6106a7c73b2a0121fdefd8cc03ddf17a3b8c7738
sneelesh182 <166792688+sneelesh182@users.noreply.github.com>
2024-08-19 16:55:58 +0530

    morgan logger

--- eval/index.js
+++ eval/index.js
@@ -1,24 +1,35 @@
-const express=require('express')
-const app=express()
-app.use(express.json())
-
-const path=require('path')
-const cors=require('cors')
-app.use(cors())
-const connectDB = require('./src/configs/mongoose')
-const auth = require('./src/middleware/auth')
-const userRouter = require('./src/routes/userRouter')
-const eventRouter = require('./src/routes/eventRoutes')
-app.use('/uploads',express.static(path.join(__dirname,'uploads')))
-const port=3000
-const url="mongodb+srv://test:test@cluster0.co3xz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-app.use('/user',userRouter)
-app.use('/userEvent',auth,eventRouter)
-app.listen(port,async()=>{
-    try{
-        await connectDB(url)
-        console.log(`server is running on http://localhost:${port}`)
-    }catch(err){
-        console.log(err)
-    }
-})
+const express = require("express");
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+app.use(express.json());
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms", {
+    stream: accessLogStream,
+  })
+);
+const cors = require("cors");
+app.use(cors());
+const connectDB = require("./src/configs/mongoose");
+const auth = require("./src/middleware/auth");
+const userRouter = require("./src/routes/userRouter");
+const eventRouter = require("./src/routes/eventRoutes");
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+const port = 3000;
+const url =
+  "mongodb+srv://test:test@cluster0.co3xz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+app.use("/user", userRouter);
+app.use("/userEvent", auth, eventRouter);
+app.listen(port, async () => {
+  try {
+    await connectDB(url);
+    console.log(`server is running on http://localhost:${port}`);
+  } catch (err) {
+    console.log(err);
+  }
+});
